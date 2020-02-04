9821db53524da58e6b21afe1fb3c90a335837135
smapl <a.mustafin00@mail.ru>
2020-02-04 15:58:13 +0300

    rename folders

--- project/__main__.py
+++ project/__main__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from warprodaction.core import app
-import warprodaction.views
+from asmanity.core import app
+import asmanity.views
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

4d0ce35662f5f26f7d1d7ccd0eafe0717aac0b14
smapl <a.mustafin00@mail.ru>
2020-02-05 18:41:48 +0300

    upfate app & add static_foldaer & add template_folder

--- server/ashmanity/src/core.py
+++ server/ashmanity/src/core.py
@@ -1,3 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder=".~/messenger/simple_messanger/src/src/static",
+    template_folder=".~/messenger/simple_messanger/src/src",
+)
+

e9fd0141f30dd90e0ffd6e76526d20ce536bcdf5
smapl <a.mustafin00@mail.ru>
2020-02-06 14:10:51 +0300

    fix route to templates&static

--- server/ashmanity/src/core.py
+++ server/ashmanity/src/core.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(
     __name__,
-    static_folder=".~/messenger/simple_messanger/src/src/static",
-    template_folder=".~/messenger/simple_messanger/src/src",
+    static_folder="~/messenger/simple_messanger/src/src/static",
+    template_folder="~/messenger/simple_messanger/src/src",
 )
 

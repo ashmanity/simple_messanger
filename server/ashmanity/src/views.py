c1a48b39bfb126628179ab5f6ab47fc4e6d26059
smapl <a.mustafin00@mail.ru>
2020-02-15 22:47:36 +0300

    add method post

--- server/ashmanity/src/views.py
+++ server/ashmanity/src/views.py
@@ -2,8 +2,21 @@ from flask import Flask, jsonify
 from random import randint
 from .core import app
 
+from pymongo import MongoClient
 
-@app.route("/")
+client = MongoClient()
+bd = client["Autorization"]
+collection = bd["Users"]
+
+
+@app.route("/", methods=["GET"])
 def main_page():
     data = {"number": randint(1, 100)}
-    return jsonify(data)
+    return jsonify(data), 200
+
+
+@app.route("/", methods=["POST"])
+def take_user_data(user_data):
+    collection.insert_one(user_data)
+    return 200
+
